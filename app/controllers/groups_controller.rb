98b49b994b989c5e6028941f10d4e2f659fd63c0
Lisa-Maria Mehta <jplisamaria@gmail.com>
2014-03-19 11:13:07 -0400

    Adds better functioning private groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,6 +21,7 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+    @images = @group.images
   end
 
   private
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,11 @@
 class Group < ActiveRecord::Base
+  ##join table
   has_many :group_memberships
   has_many :members, through: :group_memberships
+
+  ##another join table
+  has_many :group_images
+  has_many :images, through: :group_images
 
   validates :name, presence: true
 
